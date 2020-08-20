e605d8d9bc7d269a3376932418dc29f97b56274f
Katya <erina.tkachenko@yandex.ru>
2020-08-20 19:13:50 +0300

    Amplitude work

--- Get_amplitude.py
+++ Get_amplitude.py
@@ -5,7 +5,7 @@ osc = OSC_DCO7054B.Osc('192.168.10.104')
 time = {}
 data = {}
 try:
-    for i in range(1, 2):
+    for i in range(1, 5):
         time[i], data[i] = OSC_DCO7054B.Osc.get_data(osc, i)
 except:
     print('No signal in this channell')
@@ -18,12 +18,14 @@ plt.show()
 
 plt.figure()
 for key in data.keys():
-    maximum = max(data)
-    minimum = min(data)
+    maximum = max(data[key])
+    minimum = min(data[key])
+    print(maximum, minimum)
     noise = (maximum - minimum) / 6 / 4
+    print(noise)
     list_maxs = []
     list_mins = []
-    for j in data:
+    for j in data[key]:
         if maximum - j < 2 * noise:
             list_maxs.append(j)
             edge_left = len(list_mins)
@@ -31,10 +33,11 @@ for key in data.keys():
             list_mins.append(j)
     size_max = len(list_maxs)
     size_min = len(list_mins)
-    del list_maxs[0:(size_max * 0.15)]
-    del list_maxs[(len(list_maxs) - size_max * 0.15):-1]
-    del list_mins[edge_left:(edge_left + size_min * 0.15)]
-    del list_mins[(edge_left - size_min * 0.15):edge_left]
+    print(size_min, size_max)
+    del list_maxs[0:int(size_max * 0.15)]
+    del list_maxs[(len(list_maxs) - int(size_max * 0.15)):]
+    del list_mins[edge_left:int(edge_left + size_min * 0.15)]
+    del list_mins[int(edge_left - size_min * 0.15):edge_left]
     plt.plot(list_maxs)
     plt.plot(list_mins)
     amplitude[key] = (sum(list_maxs) / len(list_maxs)) - (sum(list_mins) / len(list_mins))
